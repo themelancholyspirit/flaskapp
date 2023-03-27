b7817e43e9735f26ea043cdfa62b92540aac3a61
Guga <Masterrrguga@gmail.com>
2023-03-27 21:19:13 +0400

    First commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+users = [
+    {
+        'name': 'Guga',
+        'age': 21,
+        'occupation': 'Computer Science'
+    },
+    {
+        'name': 'David',
+        'age': 21,
+        'occupation': 'Medicine'
+    }
+]
+
+
+@app.get('/')
+def hello_world():
+    return 'Hello, World'
+
+@app.get('/users')
+def get_users():
+    return jsonify(users)
